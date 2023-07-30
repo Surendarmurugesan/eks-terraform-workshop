fce2156a849928185d79d597bf21d006ebdc1dbb
Surendar <surendarmurugesan96@gmail.com>
2023-07-30 16:20:48 +0200

    naming convention and main file preparation

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+module "state_locking" {
+    source = "./env/state_locking_and_s3"
+}
+
+module "vpc_network" {
+    source = "./env/net"
+}
+
+module "iam" {
+    source = "./env/iam"
+}
+
+module "vpc_peering" {
+    source = "./env/vpc_peering"
+}
+
 module "cicd" {
     source = "./env/cicd"
 }
